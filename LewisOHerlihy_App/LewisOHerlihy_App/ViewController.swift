7059fa51f6e3542bfa5676538737831927e75b50
Lewissssss <48773485+Lewissssss@users.noreply.github.com>
2019-12-29 23:07:14 +0000

    Add files via upload

--- LewisOHerlihy_App/LewisOHerlihy_App/ViewController.swift
+++ LewisOHerlihy_App/LewisOHerlihy_App/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, viewDelegate {
         // print("hello")
         let ballView = UIImageView(image: nil)
         ballView.image = UIImage(named: "ball.png")
-        ballView.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+        ballView.frame = CGRect(x: 27, y: H*0.5, width: 45, height: 45)
         self.view.addSubview(ballView)
         
         
@@ -31,9 +31,12 @@ class ViewController: UIViewController, viewDelegate {
         
     }
 
+    @IBOutlet weak var BallDelegate: DragImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        BallDelegate.myDelegate = self
         
         //var ballObject :UIImageView
         //ballObject = UIImageView(frame:CGRect(x: 0, y: 0, width: 45, height: 45));
